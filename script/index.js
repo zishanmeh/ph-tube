265fed3dab171438f4a526dfec205e9f97e4ebde
zishanmeh <zishanmehedihasan2@gmail.com>
2024-10-02 22:18:00 +0100

    Designed card and add some functionality

--- script/index.js
+++ script/index.js
@@ -59,16 +59,39 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact shadow-xl";
     card.innerHTML = `
-         <figure>
+         <figure class="h-[200px] relative">
     <img
       src=${video.thumbnail}
+      class="h-full w-full object-cover"
       alt="Shoes" />
+      ${
+        video.others.posted_date?.length === 0
+          ? ""
+          : `<span class="absolute right-2 bottom-2 bg-black text-white rounded-lg p-1">
+            ${video.others.posted_date}
+          </span>`
+      }
+      
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+    <img class="w-10 h-10 rounded-full object-cover" src=${
+      video.authors[0].profile_picture
+    }/>
+    </div>
+    <div>
+    <h2 class="font-bold">${video.title}</h2>
+    <div class="flex items-center gap-2">
+    <p class="text-gray-400">${video.authors[0].profile_name}</p>
+    ${
+      video.authors[0].verified === true
+        ? '<img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"'
+        : ""
+    }
+    
+
+    </div>
+    <p></p>
     </div>
   </div>
     `;
