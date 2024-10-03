a0ed8bdd2c051897479aa82e861556fd3f837221
zishanmeh <zishanmehedihasan2@gmail.com>
2024-10-03 01:41:59 +0100

    Dynamically loaded data by categories

--- script/index.js
+++ script/index.js
@@ -17,7 +17,13 @@ function getTime(time) {
     return `${hour} hour ${minute} minutes ${remaining_seconds} seconds ago`;
   }
 }
-
+// Removing active class
+const removeActiveClass = () => {
+  const buttons = document.querySelectorAll(".category-btn");
+  for (let btn of buttons) {
+    btn.classList.remove("active");
+  }
+};
 // Fetch catagories and show
 
 // Create load catagories
@@ -36,87 +42,92 @@ const loadVideos = () => {
     .catch((err) => console.log(err));
 };
 
+// Create display videos
+const displayVideos = (videos) => {
+  const videoContianer = document.getElementById("videos");
+  videoContianer.innerHTML = "";
+  if (videos.length < 1) {
+    videoContianer.innerHTML = `<div class="flex justify-center items-center gap-4 flex-col col-span-full">
+    <img src="assets/img/icon.png "/>
+    <h2 class="text-center font-bold text-3xl text-gray-600">Oops!! Sorry, There is no<br/>content here</h2>
+    </div>`;
+    return;
+  }
+  videos.forEach((video) => {
+    const card = document.createElement("div");
+    card.classList = "card card-compact shadow-xl";
+    card.innerHTML = `
+           <figure class="h-[200px] relative">
+      <img
+        src=${video.thumbnail}
+        class="h-full w-full object-cover"
+        alt="Shoes" />
+        ${
+          video.others.posted_date?.length === 0
+            ? ""
+            : `<span class="absolute right-2 bottom-2 bg-black text-white rounded-lg p-1 text-xs">
+              ${getTime(video.others.posted_date)}
+            </span>`
+        }
+        
+    </figure>
+    <div class="px-0 py-2 flex gap-2">
+      <div>
+      <img class="w-10 h-10 rounded-full object-cover" src=${
+        video.authors[0].profile_picture
+      }/>
+      </div>
+      <div>
+      <h2 class="font-bold">${video.title}</h2>
+      <div class="flex items-center gap-2">
+      <p class="text-gray-400">${video.authors[0].profile_name}</p>
+      ${
+        video.authors[0].verified === true
+          ? '<img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"'
+          : ""
+      }
+      
+  
+      </div>
+      <p></p>
+      </div>
+    </div>
+      `;
+
+    videoContianer.append(card);
+  });
+};
+//load category videos
+const loadCategoryVideos = (id) => {
+  //   load videos
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // Removing active class
+      removeActiveClass();
+      const activeBtn = document.getElementById(`btn-${id}`);
+
+      activeBtn.classList.add("active");
+      displayVideos(data.category);
+    })
+    .catch((err) => console.log(err));
+};
 //Create display catagories
 
 const displayCatagoris = (categories) => {
   const categories_container = document.getElementById("categories");
   categories.forEach((item) => {
     // Create a button
-    const button = document.createElement("button");
-    button.classList = "btn";
-    button.innerText = item.category;
-
-    // Add button to category
-    categories_container.append(button);
-  });
-};
-
-const demoCARD = {
-  category_id: "1001",
-  video_id: "aaaa",
-  thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
-  title: "Shape of You",
-  authors: [
-    {
-      profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
-      profile_name: "Olivia Mitchell",
-      verified: "",
-    },
-  ],
-  others: {
-    views: "100K",
-    posted_date: "16278",
-  },
-  description:
-    "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
-};
-
-// Create display videos
-const displayVideos = (videos) => {
-  const videoContianer = document.getElementById("videos");
-  videos.forEach((video) => {
-    console.log(video);
-    const card = document.createElement("div");
-    card.classList = "card card-compact shadow-xl";
-    card.innerHTML = `
-         <figure class="h-[200px] relative">
-    <img
-      src=${video.thumbnail}
-      class="h-full w-full object-cover"
-      alt="Shoes" />
-      ${
-        video.others.posted_date?.length === 0
-          ? ""
-          : `<span class="absolute right-2 bottom-2 bg-black text-white rounded-lg p-1">
-            ${getTime(video.others.posted_date)}
-          </span>`
-      }
-      
-  </figure>
-  <div class="px-0 py-2 flex gap-2">
-    <div>
-    <img class="w-10 h-10 rounded-full object-cover" src=${
-      video.authors[0].profile_picture
-    }/>
-    </div>
-    <div>
-    <h2 class="font-bold">${video.title}</h2>
-    <div class="flex items-center gap-2">
-    <p class="text-gray-400">${video.authors[0].profile_name}</p>
-    ${
-      video.authors[0].verified === true
-        ? '<img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"'
-        : ""
-    }
-    
-
-    </div>
-    <p></p>
-    </div>
-  </div>
+    const buttonContainer = document.createElement("div");
+    // button.innerText = item.category;
+    buttonContainer.innerHTML = `
+    <button class="btn category-btn" id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})">
+    ${item.category}
+    </button>
     `;
 
-    videoContianer.append(card);
+    // Add button to category
+    categories_container.append(buttonContainer);
   });
 };
 
